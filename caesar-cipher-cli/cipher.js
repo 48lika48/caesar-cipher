d2c13610c73e7b7d270fd4abeea9df5469694309
48lika48 <48lika48@mail.ru>
2020-10-04 19:27:42 +0300

    feat: add cipher

---
+++ caesar-cipher-cli/cipher.js
@@ -0,0 +1,26 @@
+const caesar = (str, shift) => {
+    return str.split('').map(letter => {
+        let text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let textStr = text.split('');
+        let upperCaseLetter = letter.toUpperCase();
+        let index = textStr.indexOf(upperCaseLetter);
+        if (index < 0) {
+            return letter;
+        } else {
+            let upperLetter = letter == upperCaseLetter; 
+            let indexShift = (index + shift) % textStr.length;
+            if (indexShift < 0) {
+                indexShift = indexShift + textStr.length;
+            }
+            let capitalLetter = textStr[indexShift];
+            if (upperLetter) {
+                return capitalLetter;
+            } else {
+                return capitalLetter.toLowerCase();
+            }
+        }
+    }).join('');
+};
+
+console.log(caesar('ABdexYZ', 3));
+console.log('ABdexYZ');
